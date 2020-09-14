a56a68f57da386684b3c47e83a5b8573a32391fe
Vaibhav Sharma <vbvsharma.work@gmail.com>
2020-09-14 22:29:41 +0530

    Solved Stupid Machine in Java

---
+++ codechef/STUPMACH/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int T = input.nextInt();
+        while (T-- > 0) {
+            int N = input.nextInt();
+
+            int runningMin = Integer.MAX_VALUE;
+            int total = 0;
+
+            while (N-- > 0) {
+                int S = input.nextInt();
+                runningMin = Math.min(runningMin, S);
+
+                total += runningMin;
+            }
+
+            System.out.println(total);
+        }
+        input.close();
+    }
+}
