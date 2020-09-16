5a7d68711881164ca9488707a7388164bcd73855
Vaibhav Sharma <vbvsharma.work@gmail.com>
2020-09-16 17:08:05 +0530

    Solved 53. Maximum Subarray in Java

---
+++ leetcode/maximum-subarray/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        
+        int maxSum = nums[0], sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(nums[i], sum+nums[i]);
+            maxSum = Math.max(sum, maxSum);
+        }
+        
+        return maxSum;
+    }
+}
