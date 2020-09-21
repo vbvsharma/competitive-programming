bb3007b181c481e42eea9481ea783a6c2d0b708a
Vaibhav Sharma <vbvsharma.work@gmail.com>
2020-09-21 10:36:09 +0530

    Solved Solve Me First in Java

---
+++ hackerrank/practice/Algorithms/Warmup/solve-me-first/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SolveMeFirst {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        input.close();
+
+        System.out.println(solveMeFirst(a, b));
+    }
+
+    private static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+}
