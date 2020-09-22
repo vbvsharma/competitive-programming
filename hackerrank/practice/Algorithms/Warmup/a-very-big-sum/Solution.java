1efbea67234fc47da0bc2e7e762b6aae1ebf1024
Vaibhav Sharma <vbvsharma.work@gmail.com>
2020-09-22 10:52:58 +0530

    Solved A Very Big Sum in Java

---
+++ hackerrank/practice/Algorithms/Warmup/a-very-big-sum/Solution.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int n = input.nextInt();
+        long[] ar = new long[n];
+        
+        for (int i = 0; i < n; i++)
+            ar[i] = input.nextLong();
+        
+        input.close();
+        
+        System.out.println(veryBigSum(ar));
+    }
+
+    private static long veryBigSum(long[] ar) {
+        long sum = 0;
+        
+        for (int i = 0; i < ar.length; i++)
+            sum += ar[i];
+        
+        return sum;
+    }
+}
