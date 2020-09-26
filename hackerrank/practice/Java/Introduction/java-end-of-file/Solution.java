46b0ae3b951ee70974f8de3f66b10374a4de13e3
Vaibhav Sharma <vbvsharma.work@gmail.com>
2020-09-26 18:56:25 +0530

    Solved Java End-of-file

---
+++ hackerrank/practice/Java/Introduction/java-end-of-file/Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int i = 1;
+        while (input.hasNext()) {
+            System.out.println(i + " " + input.nextLine());
+            i++;
+        }
+
+        input.close();
+    }
+}
