0d9353b7c489ac33d58d2fa45b042ceda29c3752
Vaibhav Sharma <vbvsharma.work@gmail.com>
2021-01-27 09:02:21 +0530

    Solved 287. Find the Duplicate Number in Java

--- leetcode/0287-find-the-duplicate-number/another-solution-1/Solution.java
+++ leetcode/0287-find-the-duplicate-number/another-solution-1/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int findDuplicate(int[] nums) {
         int n = nums.length-1;
         
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < n; i++) {
             int index = Math.abs(nums[i]) - 1;
             
             if (nums[index] < 0)
