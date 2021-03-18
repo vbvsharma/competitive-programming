83a9b617faac2d64677f51ce906e83058a3368bd
Vaibhav Sharma <vbvsharma.work@gmail.com>
2021-03-18 07:12:24 +0530

    Solved 206. Reverse Linked List in Python3

---
+++ leetcode/0206-reverse-linked-list/solution.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head == None or head.next == None:
+            return head
+        
+        reversedList = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        
+        return reversedList
